38c894f925bf505ad734094b88074bfc806efb73
Kyle Barkins <kyle@kylebarkins.com>
2013-10-25 17:14:47 -0400

    created and editted language files to change groups to resource centers

--- plugins/bp-custom.php
+++ plugins/bp-custom.php
@@ -1,5 +1,11 @@
 <?php 
-/*Configs for Buddpress customization */
+/*
+Plugin Name: Tapp Buddypress Customizations
+Description: Includes function customizations to for buddypress.
+Version: 1.0
+Author: Kyle Barkins
+License: Public Domain
+*/
 
 define ( ‘BP_GROUPS_SLUG’, ‘resource-centers’ );
 
